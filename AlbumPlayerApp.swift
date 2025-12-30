10416f57489142cc5fde341ca5b892a1f9ace6a2
mulln <153391808+mulln@users.noreply.github.com>
2025-12-30 15:29:48 -0500

    Update AlbumPlayerApp.swift
    Set max minimum size

--- AlbumPlayerApp.swift
+++ AlbumPlayerApp.swift
@@ -5,6 +5,8 @@ struct AlbumPlayerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .frame(minWidth: 600, minHeight: 560)
         }
+        .windowStyle(.automatic)
     }
 }
